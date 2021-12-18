4d2b1d32b468e5a8a030baf35d1ebdf76c8386fd
Mger <vanyakubarin@mail.ru>
2021-12-19 00:10:25 +0300

    finished result

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,3 @@
-import { OAuth2Client } from 'google-auth-library';
 import { validateAccessToken } from '../controllers/token.js';
 
 const auth = async (req, res, next) => {
@@ -6,25 +5,9 @@ const auth = async (req, res, next) => {
         const token = req.headers.authorization?.split(' ')[1];
         if (!token) next(res.status(401).json({ message: 'User is not logged in' }))
 
-        const isCustomAuth = token.length < 500;
-
-       // let decodedData;
-
-        // if (token && isCustomAuth) {
-            const decodedData = validateAccessToken(token)
-            if (!decodedData) return next(res.status(401).json({ message: 'Invalid credentials' }))
-            req.user = { email: decodedData.email, id: decodedData.id } // why do we need this?
-        // } else {
-        //     const client = new OAuth2Client()
-        //     decodedData = (await client.verifyIdToken({
-        //         idToken: token,
-        //         audience: process.env.GOOGLE_CLIENT_ID,
-        //     })).getPayload()
-
-        //     if (decodedData?.iss !== 'accounts.google.com' && decodedData?.aud !== GOOGLE_CLIENT_ID) return next(res.status(400).json({ message: 'Invalid credentials' }))
-
-        //     req.user = { name: decodedData.name, email: decodedData.email, picture: decodedData.picture }
-        // }
+        const decodedData = validateAccessToken(token)
+        if (!decodedData) return next(res.status(401).json({ message: 'Invalid credentials' }))
+        req.user = { email: decodedData.email, id: decodedData.id } // why do we need this?
 
         next()
     } catch (error) {
