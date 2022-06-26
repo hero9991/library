af2ff66bf2192494499dc5c36db69fa111bca959
Mger <vanyakubarin@mail.ru>
2022-06-26 15:16:24 +0300

    asdf

--- client/src/components/footer/rules/Rules.tsx
+++ client/src/components/footer/rules/Rules.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import s from './Rules.module.css'
+//import s from './Rules.module.css'
 
 const Rules = ({ isRulesModal, setIsRulesModal }: Props) => {
     const hidePopup = () => setIsRulesModal(false)
 
     return (
-        <div onClick={hidePopup} className={isRulesModal ? `${s.rulesModal} ${s.active}` : s.rulesModal}>
-            <div onClick={e => e.stopPropagation()} className={isRulesModal ? `${s.rulesModalContent} ${s.active}` : s.rulesModalContent}>
+        <div onClick={hidePopup} >
+            <div onClick={e => e.stopPropagation()} >
                 Библиотека не подразумевает передачи книг в распоряжение или владение пользователям и читателям. Чтение книг может происходить лишь через веб-приложение для чтения на сайте, либо через приложения для мобильных и других устройств. Вся организация личных каталогов книг (полок) происходит исключительно внутри самого сервиса.
             </ div>
         </ div>
